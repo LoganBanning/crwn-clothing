4083d76b62f5e3d4f8f550d5e7876f3edc637030
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-02-28 16:32:22 -0700

    set up products context folder and products/shop components, added some styling to products page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context.jsx';
+import { ProductsProvider } from './contexts/products.context.jsx';
 
 import './index.scss';
 import App from './App';
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
     <UserProvider>
+    <ProductsProvider>
     <App />
+    </ProductsProvider>
     </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
