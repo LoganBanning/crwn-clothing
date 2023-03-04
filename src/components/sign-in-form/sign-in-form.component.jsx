1691e5fbb2f7d8611195f7540b67b870101de447
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-03-03 17:56:45 -0700

    set up button scss as styled component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
-import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword  } from "../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword  } from "../../utils/firebase/firebase.utils";
 
 import './sign-in-form.styles.scss';
 
@@ -28,7 +28,7 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const { user } = await signInAuthUserWithEmailAndPassword(email, password);
+      await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch(error) {
       switch(error.code) {
@@ -73,7 +73,12 @@ const SignInForm = () => {
         />
         <div className='buttons-container'>
         <Button type="submit">Sign In</Button>
-        <Button type='button' buttonType={'google'} onClick={signInWithGoogle}>Google sign in</Button>
+        <Button 
+        type='button' 
+        buttonType={BUTTON_TYPE_CLASSES.google} 
+        onClick={signInWithGoogle}>
+        Sign In With Google
+        </Button>
         </div>
       </form>
     </div>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,32 +1,39 @@
 import { createContext, useEffect, useState } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
-  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+    );
 
   if(existingCartItem) {
-    return cartItems.map((cartItem) => cartItem.id === productToAdd.id 
+    return cartItems.map((cartItem) => 
+    cartItem.id === productToAdd.id 
     ? {...cartItem, quantity: cartItem.quantity + 1}
     : cartItem
     );
   }
+
   return [...cartItems, {productToAdd, quantity: 1}];
-} 
+};
 
 const removeCartItem = (cartItems, cartItemToRemove) => {
-  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+    );
 
   if(existingCartItem.quantity === 1){
-    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
   }
 
-  return cartItems.map((cartItem) => cartItem.id === cartItemToRemove.id 
+  return cartItems.map((cartItem) => 
+  cartItem.id === cartItemToRemove.id 
     ? {...cartItem, quantity: cartItem.quantity - 1}
     : cartItem
     );
 }
 
 const clearCartItem = (cartItems, cartItemToClear) => {
-  return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 }
 
 export const CartContext = createContext({
@@ -49,12 +56,18 @@ export const CartProvider = ({children}) => {
   const [ cartTotal, setCartTotal ] = useState(0);
 
   useEffect(() => {
-    const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity, 
+      0
+      );
     setCartCount(newCartCount);
   }, [cartItems]);
 
   useEffect(() => {
-    const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price, 
+      0
+      );
     setCartTotal(newCartTotal);
   }, [cartItems]);
 
@@ -70,7 +83,16 @@ export const CartProvider = ({children}) => {
     setCartItems(clearCartItem(cartItems, cartItemToClear));
   }
 
-  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemToCart, clearItemFromCart, cartTotal};
+  const value = {
+    isCartOpen, 
+    setIsCartOpen, 
+    addItemToCart, 
+    cartItems, 
+    cartCount, 
+    removeItemToCart, 
+    clearItemFromCart, 
+    cartTotal,
+  };
 
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>
