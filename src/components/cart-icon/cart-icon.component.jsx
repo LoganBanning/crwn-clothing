5d97024e1d4aff5ff5efd31100ce19c9b6a481ce
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-03-28 17:45:00 -0600

    fixed a couple issues in the redux store and cart icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,17 +1,15 @@
 import './cart-icon.styles.scss';
 import { BsBag } from 'react-icons/bs';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectCartCount, selectIsCartOpen, selectIsCartOpen } from '../../store/cart/cart.selector';
+import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector';
 import { setIsCartOpen } from '../../store/cart/cart.action';
 
 const CartIcon = () => {
-
   const dispatch = useDispatch();
-
-  const cartCount = dispatch(useSelector(selectCartCount));
   const isCartOpen = useSelector(selectIsCartOpen);
+  const cartCount = useSelector(selectCartCount);
 
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
--- src/store/store.js
+++ src/store/store.js
@@ -8,13 +8,7 @@ const loggerMiddleware = (store) => (next) => (action) => {
     return next(action);
   }
 
-  console.log('type', action.type);
-  console.log('payload', action.payload);
-  console.log('currentState: ', store.getState());
-
   next(action);
-
-  console.log('nextState', store.getState());
 }
 
 const middleWares = [loggerMiddleware];
